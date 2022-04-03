8a9c9681443f10bf4aaf938c19badf63a84ef180
AngooBang <90963266+AngooBang@users.noreply.github.com>
2022-04-03 18:01:36 +0900

    feat : add Item in Inventory
    feat: 필드에서 습득하거나 상점에서 거래할때 사용될 addItem구현. (backspace입력으로 포션 인벤토리에 추가시킬수 있음.)

--- Assets/Scripts/UI/Item/InventorySystem.cs
+++ Assets/Scripts/UI/Item/InventorySystem.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class InventorySystem : MonoBehaviour
 {
+
+    public GameObject SlotsObject;
+
+    public GameObject testItemPrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +16,56 @@ public class InventorySystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Backspace))
+        {
+
+            AddItem(Instantiate(testItemPrefab));
+        }
+    }
+
+    void AddItem(GameObject AddItemObject)
+    {
+        ItemData addItemData = AddItemObject.GetComponent<ItemData>();
+
+        foreach (Transform childSlot in SlotsObject.transform)
+        {
+            ItemData childSlotItemData = childSlot.gameObject.GetComponentInChildren<ItemData>();
+            // 같은 아이템이 있는지 있다면 겹칠수 있는지.
+            if (childSlotItemData != null)
+            {
+                if(childSlotItemData.itemID == addItemData.itemID)
+                {
+                    int leftStack = childSlotItemData.maxStack - childSlotItemData.itemValue;
+
+                    if(leftStack >= addItemData.itemValue)
+                    {
+                        childSlotItemData.itemValue += addItemData.itemValue;
+                        Destroy(addItemData.gameObject);
+                        return;
+                    }
+                    else
+                    {
+                        // 잔여공간 부족? 그냥 빈곳에 넣자.
+                    }
+                }
+            }
+        }
+        // 같은 아이템이 없었거나 넣을수 없었다면 빈곳을 찾아 넣자.
+        foreach (Transform childSlot in SlotsObject.transform)
+        {            
+            // 빈 슬롯이라면.
+            if(childSlot.gameObject.GetComponentInChildren<ItemData>() == null)
+            {
+                AddItemObject.transform.SetParent(childSlot);
+                AddItemObject.transform.localPosition = Vector3.zero;
+                AddItemObject.transform.localScale= new Vector3(0.2f, 0.2f, 0.2f);
+                AddItemObject.name = "Item";
+                return;
+            }
+        }
+
+        // 빈 공간이 없어서 여기까지오면. 로그를띄워보자.
+        Debug.Log("인벤토리에 빈 공간이 없습니다.");
+        Destroy(AddItemObject);
     }
 }
