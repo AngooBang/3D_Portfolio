a2c5a7d58cdc6b67dae1c5afb2810865d81adecd
AngooBang <90963266+AngooBang@users.noreply.github.com>
2022-04-21 19:53:49 +0900

    feat: Create SkillPanel
    feat: UI에 스킬패널 생성 및 드래그기능.

---
+++ Assets/Scripts/UI/Skill/SkillData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillData : MonoBehaviour
+{
+    public int skillID;
+    public string skillName;
+    public string skillDescription;
+}
